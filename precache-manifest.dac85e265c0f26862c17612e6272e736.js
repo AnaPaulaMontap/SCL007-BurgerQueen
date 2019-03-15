920c47e558ad7a08ca51b5a9a76693a83bc536cd
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-03-15 13:34:45 -0300

    Updates

---
+++ precache-manifest.dac85e265c0f26862c17612e6272e736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "598771e34a3eabfd3363",
+    "url": "/SCL007-BurgerQueen/static/css/main.7007ef54.chunk.css"
+  },
+  {
+    "revision": "598771e34a3eabfd3363",
+    "url": "/SCL007-BurgerQueen/static/js/main.598771e3.chunk.js"
+  },
+  {
+    "revision": "c1c8bf55190dd5db1cbe",
+    "url": "/SCL007-BurgerQueen/static/js/runtime~main.c1c8bf55.js"
+  },
+  {
+    "revision": "d2d95a6b30608d3b231a",
+    "url": "/SCL007-BurgerQueen/static/js/2.d2d95a6b.chunk.js"
+  },
+  {
+    "revision": "b40cdfb91f89b5d39bcdf7d6c8ddaf2b",
+    "url": "/SCL007-BurgerQueen/static/media/BQ.b40cdfb9.png"
+  },
+  {
+    "revision": "0c87430265e167e6a088b1ce73d4c4a9",
+    "url": "/SCL007-BurgerQueen/index.html"
+  }
+];
